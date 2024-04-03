c9ab43469e3489b2d50b6b41ad7c9ca44c948905
Yannick Schössow <y.schoessow@ostfalia.de>
2024-04-03 12:44:11 +0200

    Attached Process to IPC Hub

--- src/traffic_sign_detection/main.cpp
+++ src/traffic_sign_detection/main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
+#include "../common/ocMember.h"
 
 int main()
 {
-  std::cout << "Hello from Traffic Sign Detection!" << std::endl;
-  
-  return 0;
+    // ocMember represents the interface to the IPC system
+    ocMember member = ocMember(ocMemberId::Sign_Detection, "Traffic Sign Detection Process");
+
+    // Creating an ocMember alone is not enough. This call will make the
+    // ocMember try to connect to the IPC system.
+    member.attach();
+
+    // Some functionality of the ocMember is put in separate types. We grab
+    // pointers to them here so we don't have to call the getters every time.
+    ocIpcSocket* socket = member.get_socket();
+    ocLogger*    logger = member.get_logger();
+
+    while (true)
+    {
+        logger->log("No Signs detected.");
+    }
+
+    return 0;
 }
