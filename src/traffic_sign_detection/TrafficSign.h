09f58b9f4652ac87cb7ce8ed325f02da92afbbb0
Yannick Schössow <y.schoessow@ostfalia.de>
2024-04-09 22:35:01 +0200

    Update TrafficSign.h

--- src/traffic_sign_detection/TrafficSign.h
+++ src/traffic_sign_detection/TrafficSign.h
@@ -5,18 +5,11 @@
 enum class TrafficSignType : uint16_t
 {
     None = 0,
-    SpeedLimit_7,
-    SpeedLimit_10,
-    SpeedLimit_20,
-    SpeedLimit_30,
-    SpeedLimit_50,
-    SpeedLimit_60,
-    SpeedLimit_70,
-    SpeedLimit_80,
-    SpeedLimit_90,
-    SpeedLimit_100,
-    SpeedLimit_120,
+    Stop,
     GiveWay,                // Vorfahrt beachten
+    Park,
+    Left,
+    Right,
     COUNT
 };
 
