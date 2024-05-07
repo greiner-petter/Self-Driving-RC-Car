79a8922dca58eeb39d245fcf6648d8276977b165
Mika Braunschweig <m.braunschweig@ostfalia.de>
2024-05-07 15:36:08 +0200

    implement basic program to receive line data

---
+++ src/intersection_detection/main.cpp
@@ -0,0 +1,67 @@
+#include "../common/ocTypes.h"
+#include "../common/ocMember.h"
+#include <signal.h>
+#include <csignal>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/types.hpp>
+
+using cv::Point2f;
+using cv::Mat;
+using cv::Point;
+
+using std::vector;
+
+static bool running = true;
+ocLogger *logger;
+
+static void signal_handler(int)
+{
+    running = false;
+}
+
+int main() {
+    // Catch some signals to allow us to gracefully shut down the process
+    signal(SIGINT, signal_handler);
+    signal(SIGQUIT, signal_handler);
+    signal(SIGTERM, signal_handler);
+    
+
+    ocMember member(ocMemberId::Intersection_Detection, "Intersection_Detection");
+    member.attach();
+
+    ocIpcSocket *socket = member.get_socket();
+    logger = member.get_logger();
+
+    ocPacket ipc_packet;
+    ipc_packet.set_message_id(ocMessageId::Subscribe_To_Messages);
+    ipc_packet.clear_and_edit()
+        .write(ocMessageId::Lines_Available);
+    socket->send_packet(ipc_packet);
+
+    // Listen for detected Lines
+    while(running) {
+        int32_t socket_status;
+        while (0 < (socket_status = socket->read_packet(ipc_packet, false)))
+        {
+            switch(ipc_packet.get_message_id())
+                {
+                    case ocMessageId::Lines_Available:
+                    {
+                        static struct ocBevLines lines;
+                        ipc_packet.read_from_start().read(&lines);
+                        // TODO: Handle data
+                        logger->log("Got data!");
+
+                    } break;
+                    default:
+                    {
+                        ocMessageId msg_id = ipc_packet.get_message_id();
+                        ocMemberId  mbr_id = ipc_packet.get_sender();
+                        logger->warn("Unhandled message_id: %s (0x%x) from sender: %s (%i)", to_string(msg_id), msg_id, to_string(mbr_id), mbr_id);
+                    } break;
+                }
+        }
+    }
+
+    return 0;
+}
