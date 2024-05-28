2450572f5fbd637f4b07e535ce0f708612d1e5d9
Calvin Mende <c.mende@ostfalia.de>
2024-05-28 13:02:10 +0200

    add: fix

--- src/lane_detection/main.cpp
+++ src/lane_detection/main.cpp
@@ -272,8 +272,8 @@ int main()
                     ipc_packet.set_message_id(ocMessageId::Start_Driving_Task);
                     ipc_packet.clear_and_edit()
                         .write<int16_t>(36)
-                        .write<int8_t>(car_properties.front_steering_angle_to_byte(getAverageOfOldAngles()))
-                        .write<int8_t>(-car_properties.rear_steering_angle_to_byte(getAverageOfOldAngles()))
+                        .write<int8_t>(car_properties.front_steering_angle_to_byte(getAverageOfOldAngles())/2)
+                        .write<int8_t>(-car_properties.rear_steering_angle_to_byte(getAverageOfOldAngles())/2)
                         .write<uint8_t>(0x8)
                         .write<int32_t>(car_properties.cm_to_steps(1));
                     socket->send_packet(ipc_packet);
