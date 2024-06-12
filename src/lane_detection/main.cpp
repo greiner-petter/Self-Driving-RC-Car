038446302d5f4790486b640749314aebe0b8d044
Calvin Mende <c.mende@ostfalia.de>
2024-06-12 04:51:18 +0200

    test

--- src/lane_detection/main.cpp
+++ src/lane_detection/main.cpp
@@ -56,9 +56,9 @@ bool check_if_on_street(std::array<int, 25> histogram) {
 }
 
 float get_angle(int dest) {
-    float angle = (dest - 200) * 0.9; // MAPPING TO INT 8 -80 to 80 for angle
+    float angle = (dest - 200) * 0.3; // MAPPING TO INT 8 -80 to 80 for angle
 
-    return std::clamp((int) angle, -180, 180); // Clamp between -80 and 80 so tire doesn't get stuck due to too high angle (150 and -150 if back steering is enabled)
+    return std::clamp((int) angle, -60, 60); // Clamp between -80 and 80 so tire doesn't get stuck due to too high angle (150 and -150 if back steering is enabled)
 
 }
 
