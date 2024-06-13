da600810757b9e1987842f2f5eab9076c7a7d7d3
Calvin Mende <c.mende@ostfalia.de>
2024-06-13 11:27:51 +0200

    test

--- src/lane_detection/main.cpp
+++ src/lane_detection/main.cpp
@@ -11,6 +11,7 @@
 #include <tensorflow/lite/op_resolver.h>
 #include <tensorflow/lite/string_util.h>
 #include <tensorflow/lite/tools/gen_op_registration.h>
+#include <tensorflow/lite/optional_debug_tools.h>
 
 #include "../common/ocWindow.h"
 
@@ -22,46 +23,34 @@
 
 #define DEBUG_WINDOW
 
-#include <tensorflow/lite/model.h>
-#include <tensorflow/lite/optional_debug_tools.h>
-
 class Lanes {
 public:
     std::vector<cv::Mat> recent_fit;
     cv::Mat avg_fit;
 
     Lanes() {
-        avg_fit = cv::Mat::zeros(cv::Size(160, 80), CV_32FC3);
+        avg_fit = cv::Mat::zeros(cv::Size(160, 80), CV_32FC1);
     }
 };
 
 Lanes lanes;
+ocLogger *logger;
 
-cv::Mat road_lines(const cv::Mat &image, tflite::Interpreter* interpreter) {
+cv::Mat road_lines(cv::Mat &image, tflite::Interpreter* interpreter) {
     // Resize image
     cv::Mat small_img;
     cv::resize(image, small_img, cv::Size(160, 80));
 
-    // Prepare the input data
-    small_img.convertTo(small_img, CV_32FC3);
-    std::vector<float> input_data(small_img.reshape(1, 1));
-
-    // Get input and output tensors
-    int input_index = interpreter->inputs()[0];
-    int output_index = interpreter->outputs()[0];
-
-    // Set the tensor to point to the input data to be inferred
-    float* input_tensor_data = interpreter->typed_tensor<float>(input_index);
-    std::copy(input_data.begin(), input_data.end(), input_tensor_data);
+    memcpy(interpreter->typed_input_tensor<float>(0), small_img.data, small_img.total() * small_img.elemSize());
 
     // Run the inference
     interpreter->Invoke();
 
     // Get the output data
-    float* output_tensor_data = interpreter->typed_tensor<float>(output_index);
-    std::vector<float> output_data(output_tensor_data, output_tensor_data + (160 * 80 * 3));
+    float* output_tensor_data = interpreter->typed_output_tensor<float>(0);
+    std::vector<float> output_data(output_tensor_data, output_tensor_data + (160 * 80 * 1));
     
-    cv::Mat prediction(80, 160, CV_32FC3, output_data.data());
+    cv::Mat prediction(80, 160, CV_32FC1, output_data.data());
     prediction *= 255.0;
 
     // Append prediction to recent_fit
@@ -73,34 +62,42 @@ cv::Mat road_lines(const cv::Mat &image, tflite::Interpreter* interpreter) {
     }
 
     // Calculate average of recent_fit
-    lanes.avg_fit = cv::Mat::zeros(prediction.size(), CV_32FC3);
+    lanes.avg_fit = cv::Mat::zeros(prediction.size(), CV_32FC1);
     for (const auto& mat : lanes.recent_fit) {
         lanes.avg_fit += mat;
     }
     lanes.avg_fit /= lanes.recent_fit.size();
-    lanes.avg_fit.convertTo(lanes.avg_fit, CV_8UC3);
+    lanes.avg_fit.convertTo(lanes.avg_fit, CV_8UC1);
 
     // Create lane drawn image
     cv::Mat blanks = cv::Mat::zeros(lanes.avg_fit.size(), CV_8UC3);
     std::vector<cv::Mat> channels(3);
     cv::split(blanks, channels);
-    channels[1] = lanes.avg_fit;
+    channels[1] = lanes.avg_fit; // assuming green channel for lanes
     cv::merge(channels, blanks);
 
     // Resize lane drawn image
     cv::Mat lane_drawn;
     cv::resize(blanks, lane_drawn, cv::Size(400, 400));
 
+    // Convert image to 3-channel if it is not
+    cv::Mat color_image;
+    if (image.channels() == 1) {
+        cv::cvtColor(image, color_image, cv::COLOR_GRAY2BGR);
+    } else {
+        color_image = image;
+    }
+
     // Add lanes to original image
     cv::Mat result;
-    cv::addWeighted(image, 1, lane_drawn, 1, 0, result);
+    cv::addWeighted(color_image, 1, lane_drawn, 1, 0, result);
 
     return result;
 }
 
 ocMember member(ocMemberId::Lane_Detection_Values, "Lane Detection");
 
-ocLogger *logger;
+
 ocPacket ipc_packet;
 ocIpcSocket *socket;
 ocSharedMemory *shared_memory;
@@ -137,7 +134,7 @@ int main()
         .write(ocMessageId::Birdseye_Image_Available);
     socket->send_packet(ipc_packet);
 
-    const char* model_path = "model.tflite";
+    const char* model_path = "/home/calvin/Documents/gruppe2/scripts/model.tflite";
     std::unique_ptr<tflite::FlatBufferModel> model = tflite::FlatBufferModel::BuildFromFile(model_path);
     if (!model) {
         std::cerr << "Failed to load model: " << model_path << std::endl;
@@ -168,17 +165,15 @@ int main()
             {
                 case ocMessageId::Birdseye_Image_Available:
                 {
-                    cv::Mat matrix = cv::Mat(400,400,CV_8UC1, shared_memory->cam_data[0].img_buffer);
+                    cv::Mat matrix = cv::Mat(400,400,CV_8UC1, shared_memory->bev_data[0].img_buffer);
 
-                    if(std::getenv("CAR_ENV") != NULL) {
                         cv::imwrite("bev.jpg", matrix);
-                    } 
-
-                    if(std::getenv("CAR_ENV") != NULL) {
-                        cv::Mat result = road_lines(matrix, interpreter.get());
+                    
+                    
+                        cv::Mat result = road_lines(cv::imread("test.jpg"), interpreter.get());
                         cv::imwrite("bev_out.jpg", result);
-                    } 
-                    return;
+                    
+                    return 0;
                 } break;
                 default:
                     {
