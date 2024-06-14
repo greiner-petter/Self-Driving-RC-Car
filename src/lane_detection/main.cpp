8cab47ff5742288f24b427b31fc03f63cabbeec3
Calvin Mende <c.mende@ostfalia.de>
2024-06-14 16:37:19 +0200

    test

--- src/lane_detection/main.cpp
+++ src/lane_detection/main.cpp
@@ -118,9 +118,9 @@ std::pair<int, int> get_angles_from_average_angle(float average_angle) {
     if(average_angle >= 65) {
         front_angle = 65;
         back_angle = -average_angle + 65;
-    } else if(average_angle >= 5 && average_angle <= 10) {
+    } else if(average_angle >= 5 && average_angle <= 30) {
         front_angle = average_angle;
-        back_angle = 10 - average_angle;
+        back_angle = 30 - average_angle;
     } else if(average_angle < 5 && average_angle > 0) {
         front_angle = average_angle;
         back_angle = average_angle;
@@ -129,9 +129,9 @@ std::pair<int, int> get_angles_from_average_angle(float average_angle) {
     if(average_angle <= -65) {
         front_angle = -65;
         back_angle = average_angle + 65;
-    } else if(average_angle <= -5 && average_angle >= -10) {
+    } else if(average_angle <= -5 && average_angle >= -30) {
         front_angle = average_angle;
-        back_angle = -10 - average_angle;
+        back_angle = -30 - average_angle;
     } else if(average_angle > -5 && average_angle < 0) {
         front_angle = average_angle;
         back_angle = average_angle;
