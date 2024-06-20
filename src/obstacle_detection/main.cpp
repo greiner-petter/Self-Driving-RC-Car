5b05bae0ca53188bacdd15792716e12c251610fc
YanSchw <schoessow.yannick@gmail.com>
2024-06-20 16:03:42 +0200

    Fixed compile error

--- src/obstacle_detection/main.cpp
+++ src/obstacle_detection/main.cpp
@@ -54,7 +54,7 @@ int main(int argc,char* argv[])
     while (true)
     {
         // Fetch Camera Data
-        ocCamData *cam_data = &shared_memory->cam_data[s_SharedMemory->last_written_cam_data_index];
+        ocCamData *cam_data = &shared_memory->cam_data[shared_memory->last_written_cam_data_index];
 
         int type = CV_8UC1;
         if (3 == bytes_per_pixel(cam_data->pixel_format)) type = CV_8UC3;
@@ -64,7 +64,7 @@ int main(int argc,char* argv[])
         cv::Mat cam_image((int)cam_data->height, (int)cam_data->width, type);
         cam_image.data = cam_data->img_buffer;
 
-        logger->info(std::to_string(CalcObstacleCoverage(cam_image, cam_data->width, cam_data->height)).c_str());
+        logger->warn(std::to_string(CalcObstacleCoverage(cam_image, cam_data->width, cam_data->height)).c_str());
     }
 
     logger->warn("Obstacle-Detection: Process Shutdown.");
