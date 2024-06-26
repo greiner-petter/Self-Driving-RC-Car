93c8eb1aab5ac11d097594aabca25a16c76a47d5
YanSchw <schoessow.yannick@gmail.com>
2024-06-26 20:18:24 +0200

    Added obstacle_detection to Scripts

--- scripts/full_simulation.sh
+++ scripts/full_simulation.sh
@@ -9,6 +9,7 @@ $BINARY_DIR/video_viewer &
 $BINARY_DIR/image_processing_bev &
 $BINARY_DIR/lane_detection &
 $BINARY_DIR/traffic_sign_detection --nogui &
+$BINARY_DIR/obstacle_detection &
 $BINARY_DIR/intersection_detection &
 
 # Add any additional process you want to run here.
