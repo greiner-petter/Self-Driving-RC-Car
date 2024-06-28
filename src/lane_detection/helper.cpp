7ec87309fec6678469d59796e4a3291756ea5f64
Calvin Mende <c.mende@ostfalia.de>
2024-06-28 21:01:49 +0200

    test

--- src/lane_detection/helper.cpp
+++ src/lane_detection/helper.cpp
@@ -28,7 +28,7 @@ class Helper {
             }
             
             const int INITIAL_RADIUS = 50;
-            const int FINAL_RADIUS = 250;
+            const int FINAL_RADIUS = 175;
             const int ZIRKLE_DIFF = 25;
 
             cv::Point* previous_center = nullptr;
