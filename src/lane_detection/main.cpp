97e59f53bbeb77866150c35adef094499e45b838
Calvin Mende <c.mende@ostfalia.de>
2024-06-28 15:18:00 +0200

    test

--- src/lane_detection/main.cpp
+++ src/lane_detection/main.cpp
@@ -162,7 +162,7 @@ int main()
 
                     float pixelRadiusTocmRadius = 0.6 * radius;
 
-                    float front_angle = -std::clamp<float>(90 - std::asin(0.11 / pixelRadiusTocmRadius) * (180/3.14), -65, 65);
+                    float front_angle = -std::clamp<float>(std::asin(11 / pixelRadiusTocmRadius) * (180/3.14), -65, 65);
                     float back_angle = -front_angle;
 
                    
