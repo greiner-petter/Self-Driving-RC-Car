2c9e6e03e166adbed45024a32e15550bda045ef6
YanSchw <schoessow.yannick@gmail.com>
2024-06-30 22:40:50 +0200

    Fixed Linker error

--- src/decider/States/State.h
+++ src/decider/States/State.h
@@ -10,7 +10,7 @@ class State{
     public:
         virtual void on_entry(Statemachine* statemachine) = 0;
         virtual void run(Statemachine* statemachine, void *data) = 0;
-        virtual void on_exit(Statemachine* statemachine);
+        virtual void on_exit(Statemachine* statemachine) = 0;
         virtual ~State(){}
         
         inline static TrafficSignType trafficSign = TrafficSignType::None;
