13972410529af5622d497c8ee015c90dc413bcbd
YanSchw <schoessow.yannick@gmail.com>
2024-06-30 22:15:21 +0200

    Added TrafficSignTypeToString()

--- src/traffic_sign_detection/TrafficSign.h
+++ src/traffic_sign_detection/TrafficSign.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstdint>
+#include <string>
 
 enum class TrafficSignType : uint16_t
 {
@@ -12,6 +13,20 @@ enum class TrafficSignType : uint16_t
     Right,
     COUNT
 };
+
+static std::string TrafficSignTypeToString(const TrafficSignType& type)
+{
+    switch (type)
+    {
+        case TrafficSignType::None: return "None";
+        case TrafficSignType::Stop: return "Stop";
+        case TrafficSignType::PriorityRoad: return "PriorityRoad";
+        case TrafficSignType::Park: return "Park";
+        case TrafficSignType::Left: return "Left";
+        case TrafficSignType::Right: return "Right";
+        default: return "TrafficSignTypeToString(): MISSING IMPLEMENTATION.";
+    }
+}
 
 struct TrafficSign
 {
