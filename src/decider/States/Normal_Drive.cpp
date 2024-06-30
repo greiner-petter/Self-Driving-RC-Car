d3cb9d2d91ee5e3e3e8618d0786081a7dd6ed3c2
jreichstein <j.reichstein@ostfalia.de>
2024-06-30 17:56:10 +0200

    changed way normal_drive receives packages

--- src/decider/States/Normal_Drive.cpp
+++ src/decider/States/Normal_Drive.cpp
@@ -16,7 +16,8 @@ void Normal_Drive::initialize(){
         ocPacket sup = ocPacket(ocMessageId::Subscribe_To_Messages);
         sup.clear_and_edit()
             .write(ocMessageId::Intersection_Detected)
-            .write(ocMessageId::Object_Found);
+            .write(ocMessageId::Object_Found)
+            .write(ocMessageId::Lane_Detection_Values);
         socket->send_packet(sup);
 
         is_initialized = true;
@@ -42,7 +43,7 @@ void Normal_Drive::run(Statemachine* statemachine, void* data){
     
     while (true) {
        
-        int result = socket->read_packet(recv_packet, false);
+        int result = socket->read_packet(recv_packet);
         bool object_found = false;
         ocTime now = ocTime::now();
 
@@ -58,6 +59,21 @@ void Normal_Drive::run(Statemachine* statemachine, void* data){
                 case ocMessageId::Object_Found:{
                     object_found = true;
                 }break;
+
+                case ocMessageId::Lane_Detection_Values:{
+                    auto reader = recv_packet.read_from_start();
+
+                    int16_t speed = reader.read<int16_t>();
+                    int8_t steering_front = reader.read<int8_t>();
+                    int8_t steering_back = reader.read<int8_t>();
+
+                    if(object_found){
+                        Driver::stop();
+                    } else{
+                        Driver::drive_both_steering_values(speed, steering_front, steering_back);
+                    }
+                    
+                }break;
                 
                 default:{
                     ocMessageId msg_id = recv_packet.get_message_id();
@@ -67,12 +83,13 @@ void Normal_Drive::run(Statemachine* statemachine, void* data){
             }
         }
 
+        /*
         if(object_found){
             Driver::stop();
         } else{
             Driver::drive_forward();
         }
-
+        */
     }
 
     
