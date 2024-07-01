08776a6ae1e856f12dfc664fe0e46218f0289292
Oliver Greiner-Petter <github@greiner-petter.net>
2024-07-01 07:36:12 +0200

    width height

--- src/video_recorder/main.cpp
+++ src/video_recorder/main.cpp
@@ -210,8 +210,11 @@ int main(int argc, const char **argv)
                 cv::Mat image(400, 400, CV_8UC1, shared_memory->bev_data[2 | bit].img_buffer);
                 if (!video_writer.isOpened())
                 {
-                    if (!crop_hor) crop.width  = 400;
-                    if (!crop_ver) crop.height = 400;
+                    int32_t width    = (int32_t)image->width;
+                    int32_t height   = (int32_t)image->height;
+
+                    if (!crop_hor) crop.width  = width;
+                    if (!crop_ver) crop.height = height;
 
                     if (width < crop.x + crop.width || height < crop.y + crop.height)
                     {
